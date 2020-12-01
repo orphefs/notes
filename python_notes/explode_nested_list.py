0f58f5062674665d2fb51210010e3582dcc8518f
Orfeas Kypris <orphefs@users.noreply.github.com>
2020-12-01 12:42:52 +0200

    add support for strings

--- python_notes/explode_nested_list.py
+++ python_notes/explode_nested_list.py
@@ -1,10 +1,15 @@
-def explode_nested_list(l: list):
+def explode_nested_list(list_like_item):
     exploded_list = []
-    for item in l:
-        if isinstance(item, list):
-            exploded_list += explode_nested_list(item)
-        else:
-            exploded_list.append(item)
+    if isinstance(list_like_item, list):
+        for item in list_like_item:
+            if isinstance(item, list):
+                exploded_list += explode_nested_list(item)
+            else:
+                exploded_list.append(item)
+    elif isinstance(list_like_item, str):
+        exploded_list.append(list_like_item)
     return exploded_list
 
 explode_nested_list([1,2,[3,[4, [5,6,7]]]])
+explode_nested_list(["blah", "blom"])
+explode_nested_list(['Roasted Chili Corn Salsa', ['Fajita Vegetables', 'Rice', 'Black Beans', 'Cheese', 'Sour Cream']])
